4f99d91ff4baa5bd45ada4e6c93d51096d09b6f3
GhadyAbuRaya <vxytjghx7d@privaterelay.appleid.com>
2025-12-16 21:02:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+iris = load_iris()
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
+
+print(df.head())
+print(df.describe())
+df.hist(figsize=(8, 6))
+plt.tight_layout()
+plt.show()
+plt.scatter(df["sepal length (cm)"], df["petal length (cm)"])
+plt.xlabel("Sepal Length (cm)")
+plt.ylabel("Petal Length (cm)")
+plt.title("Correlation between Sepal Length and Petal Length")
+plt.savefig("correlation.png")
+plt.show()
 
